453eca4d058eae7b6451c49ab743c4d02ebc0f5b
somanbaqai <soman.baqai@gmail.com>
2018-08-23 00:39:47 +0500

    commit by soman baqai

--- services.php
+++ services.php
@@ -1,40 +1,60 @@
 <?php
 include("dbhelper.php");
 
-$_SQL_BAYANS    = "select F.id, F.title, F.filename, F.file_description, F.dlversion, F.postdate from  
-	majliseirshad_org.wp_vibh0o_download_monitor_files as F 
-WHERE F.Id IN (select download_id from majliseirshad_org.wp_vibh0o_download_monitor_relationships where taxonomy_id = ?)
-order by postDate desc LIMIT ?,?;";
+$_SQL_BAYANS    = "SELECT 
+                        F.id, F.title, F.filename, F.file_description, F.dlversion, F.postdate
+                    FROM  
+	                    majliseirshad_org.wp_vibh0o_download_monitor_files 
+                    as F WHERE 
+                        F.Id IN (SELECT
+                                         download_id 
+                                 FROM 
+                                        majliseirshad_org.wp_vibh0o_download_monitor_relationships 
+                                WHERE taxonomy_id = ?)
+                    ORDER BY  postDate desc LIMIT ?,?;";
 
-$db = DatabaseHelper::getInstance();
-$conn = $db->getConnection(); 
 
 
-/** Fetching records from DB */
-$stmt = $conn->prepare($_SQL_BAYANS);
-$param1 = 1; // category
-$param2 = 1; // from
-$param3 = 50; // to
-$stmt->bind_param("sss",$param1,$param2,$param3);
-// $result = $conn->query($_SQL_BAYANS);
-$stmt->execute();
-$result = $stmt->get_result();
-header('Content-Type: application/json');
-if ($result->num_rows > 0) {
-    $rows = array();
+if(!is_null($_GET['taxId']) && !is_null($_GET['from']) && !is_null($_GET['to'])) {    
+    /** Fetching records from DB */
 
-    while($r = $result->fetch_assoc()){        
-        $rows['result'][] = $r;
+    $db = DatabaseHelper::getInstance();
+    $conn = $db->getConnection(); 
+    
+    $stmt = $conn->prepare($_SQL_BAYANS);
+    $taxId = $_GET['taxId']; // category
+    $from = $_GET['from']; // from
+    $to = $_GET['to']; // to
+    $stmt->bind_param("iii",$taxId,$from,$to);
+    // $result = $conn->query($_SQL_BAYANS);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    header('Content-Type: application/json');
+    if ($result->num_rows > 0) {
+        $rows = array();
+    
+        while($r = $result->fetch_assoc()){        
+            $rows['result'][] = $r;
+        }
+        
+        // echo "id: " . $row["id"]. " - Title: " . $row["title"]. " " . $row["filename"]. "<br>";
+        //     while($row = $result->fetch_assoc()) {
+        // }
+        
+        print_r(json_encode($rows));
+        $stmt->close();
+    }else{    
+        http_response_code(404);
     }
     
-    // echo "id: " . $row["id"]. " - Title: " . $row["title"]. " " . $row["filename"]. "<br>";
-    //     while($row = $result->fetch_assoc()) {
-    // }
-    
-    print_r(json_encode($rows));
-    $stmt->close();
-}else{    
-    http_response_code(404);
+}else{
+    throw new Exception("Required variable not found");
+
 }
+
+
+
+
+
 
 ?>
